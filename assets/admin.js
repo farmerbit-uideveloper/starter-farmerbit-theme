0fdfd2ff31984ed535b350dced467700285bdec5
Fabio Chiarato <>
2021-03-22 11:36:56 +0100

    Wp admin : collapse all acf flexible layout by default

--- assets/admin.js
+++ assets/admin.js
@@ -5,4 +5,13 @@ function importAll (r) {
   r.keys().forEach(r)
 }
 
+/**
+ *  FC - Collapse all acf flexible layout
+ */
+ if( jQuery( '.acf-flexible-content .layout' ).length ) {
+	jQuery( '.acf-flexible-content .layout' ).each( function() {
+		! jQuery( this ).hasClass( '-collapsed' ) && jQuery( this ).addClass( '-collapsed' );
+	});
+}
+
 importAll(require.context('../Components/', true, /\/admin\.js$/))
